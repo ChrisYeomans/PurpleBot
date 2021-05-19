7c6a5013ea155ae09e43c4ad4b4a089fb5ba5612
ChrisYeomans <chrisj.yeomans@gmail.com>
2021-05-19 16:46:06 +0100

    Roles and role message updates properly

--- bot.py
+++ bot.py
@@ -2,8 +2,16 @@ import discord
 
 TEST = True
 ROLE_MESSAGE_ID = -1
-EMOJI_ROLE_DICT = {"👍": "test"}
-ROLE_COLOURS = {"test": 0xffffff}
+EMOJI_ROLE_DICT = {
+    "👍": "test",
+    "❤️": "heart",
+    "🔥": "fire",
+}
+ROLE_COLOURS = {
+    "test": 0x000000,
+    "heart": 0xf20f0f,
+    "fire": 0xe88f00,
+}
 
 def main():
     TOKEN = input()
@@ -23,25 +31,32 @@ def main():
                         async for message in tc.history(limit=10):
                             if message.author == client.user and "role" in message.content.lower():
                                 ROLE_MESSAGE_ID = message.id
+                                break
+                        await setup_reaction_message(tc)
+                        break
+                await create_colour_roles(g)
+        
         print("Bot Ready")
         print(ROLE_MESSAGE_ID)
     
     @client.event
     async def on_raw_reaction_add(payload):
-        user = payload.member
-        if user != client.user:
-            emoji = str(payload.emoji)
-            if emoji in EMOJI_ROLE_DICT:
-                await user.add_roles(discord.utils.get(user.guild.roles, name=EMOJI_ROLE_DICT[emoji]))
+        if payload.message_id == ROLE_MESSAGE_ID:
+            user = payload.member
+            if user != client.user:
+                emoji = str(payload.emoji)
+                if emoji in EMOJI_ROLE_DICT:
+                    await user.add_roles(discord.utils.get(user.guild.roles, name=EMOJI_ROLE_DICT[emoji]))
 
     @client.event
     async def on_raw_reaction_remove(payload):
-        guild = client.get_guild(payload.guild_id)
-        user = guild.get_member(payload.user_id)
-        if user != client.user:
-            emoji = str(payload.emoji)
-            if emoji in EMOJI_ROLE_DICT:
-                await user.remove_roles(discord.utils.get(guild.roles, name=EMOJI_ROLE_DICT[emoji]))
+        if payload.message_id == ROLE_MESSAGE_ID:
+            guild = client.get_guild(payload.guild_id)
+            user = guild.get_member(payload.user_id)
+            if user != client.user:
+                emoji = str(payload.emoji)
+                if emoji in EMOJI_ROLE_DICT:
+                    await user.remove_roles(discord.utils.get(guild.roles, name=EMOJI_ROLE_DICT[emoji]))
 
 
     @client.event
@@ -56,28 +71,33 @@ def main():
     # Only here for testing and fixing stuff
     @client.event
     async def on_message(message):
-        if message.content == "setup_reaction_message" and message.author.guild_permissions.administrator:
-            await setup_reaction_message(message)
-            await message.delete()
-
+        if message.author.guild_permissions.administrator:
+            if message.content == "setup_reaction_message":
+                await setup_reaction_message(message.channel)
+                await message.delete()
+            elif message.content == "update_roles":
+                print("updating roles")
+                await create_colour_roles(message.guild)
 
     client.run(TOKEN)
 
 async def setup_reaction_message(tc):
     # Build message
-    
     reaction_message = f"React to the following to get the relevant role\n"
     for i in EMOJI_ROLE_DICT:
         reaction_message += f"{i} {EMOJI_ROLE_DICT[i]}\n"
-
-    # Send message and add reactions
-    m = await tc.send(reaction_message)
+    if ROLE_MESSAGE_ID != -1:
+        m = await tc.fetch_message(ROLE_MESSAGE_ID)
+        await m.edit(content = reaction_message)
+    else:
+        m = await tc.send(reaction_message)
     for i in EMOJI_ROLE_DICT:
         await m.add_reaction(i)
+    
 
 async def create_colour_roles(guild):
     for role in ROLE_COLOURS:
-        await guild.create_role(name="role", colour=discord.Colour(ROLE_COLOURS[role]))
+        await guild.create_role(name=role, colour=discord.Colour(ROLE_COLOURS[role]))
     
 
 if __name__ == "__main__":
